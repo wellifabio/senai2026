2fffb6ae1e7e38c560e666ce7a8c6ed1dd928e64
ReenyeLima <reenyelima@hotmail.com>
2026-08-18 15:09:01 -0300

    Atividade

---
+++ ds/turmas/1DES_B/2º Semestre/03-pbe1/aula02/mockup.vendedores.js
@@ -0,0 +1,8 @@
+/*
+    {
+        "matricula":""
+        "nome": ""
+        "salario": 00000
+        "comissao": 0.5
+    }
+*/
--- ds/turmas/1DES_B/2º Semestre/03-pbe1/aula02/produtos.service.js
+++ ds/turmas/1DES_B/2º Semestre/03-pbe1/aula02/produtos.service.js
@@ -17,6 +17,49 @@ const listar = () => {
     } );
 };
 
+const buscar = (busca) => {
+    produtos.forEach( (produto) => {
+        let temp = JSON.stringify(produto).toLowerCase();
+        if(temp.includes(busca.toLowerCase())) {
+            console.log(produto);
+        }
+    } );
+};
+
+const buscarPorMarca = (busca) => {
+    produtos.forEach( (produto) => {
+        let marca = produto.marca.toLowerCase();
+
+        if(marca == busca.toLowerCase()) {
+            console.log(produto);
+        }
+    } );
+};
+
+const excluir = (indice) => {
+    produtos.splice(indice, 1);
+};
+
+const excluirPorModelo = (modelo) => {
+    produtos.forEach((produto, indice) => {
+        let modeloTemp = produto.modelo.toLowerCase();
+
+        if(modeloTemp == modelo.toLowerCase()) {
+            produtos.splice(indice, 1);
+        }
+    });
+};
+
 cadastrar("Marca Exemplo", "Modelo Teste", "2026", 15000);
+
+//listar();
+
+//buscar("gt");
+
+//buscarPorMarca("mustang");
+
+//excluir(1);
+
+excluirPorModelo("m10");
 
 listar();

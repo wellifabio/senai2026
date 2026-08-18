abaa5c7c663471679f4508c86e63908b355b92f5
ReenyeLima <reenyelima@hotmail.com>
2026-08-18 08:32:40 -0300

    PBE1 18-08

---
+++ ds/turmas/1DES_A/2º Semestre/03-pbe1/aula02/mockup.vendedores.js
@@ -0,0 +1,18 @@
+/*
+
+Matrícula
+Nome
+Salario
+Comissão
+
+Mockup
+4 Cadastros
+
+Service
+Cadastrar, Listar, Excluir, Buscar, 
+Buscar Por Matricula, Buscar Por Nome,
+Excluir por Matricula
+
+Fluxograma
+
+*/
--- ds/turmas/1DES_A/2º Semestre/03-pbe1/aula02/produtos.service.js
+++ ds/turmas/1DES_A/2º Semestre/03-pbe1/aula02/produtos.service.js
@@ -8,7 +8,34 @@ const cadastrar = (marca, modelo, quantidade, preco) => {
         preco
     };
 
-    console.log(produto);
+    produtos.push(produto);
+};
+
+const listar = () => {
+    produtos.forEach((produto, indice) => {
+        console.log(indice, produto);
+    });
+};
+
+const buscar = (busca) => {
+    let resultado = [];
+
+    produtos.forEach((produto) => {
+        if(JSON.stringify(produto).toLowerCase().includes(busca.toLowerCase())) {
+            resultado.push(produto);
+        }
+    });
+
+    //return resultado;
+    console.log(resultado);
+};
+
+const excluir = (indice) => {
+    produtos.splice(indice, 1);
 };
 
 cadastrar("Samsung", "a36", 20, 1800);
+listar();
+buscar("1800");
+excluir(0);
+listar();

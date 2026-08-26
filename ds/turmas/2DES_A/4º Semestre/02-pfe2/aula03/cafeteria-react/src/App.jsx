559dfc6580f66646ce19d557467da6a346b22956
robsonbsouzaa <robsonbacchindesouza@gmail.com>
2026-08-26 07:50:01 -0300

    up

---
+++ ds/turmas/2DES_A/4º Semestre/02-pfe2/aula03/cafeteria-react/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+import Banner from './components/Banner'
+import Sobre from './components/Sobre'
+
+function App(){
+    return (
+        <>
+        <Header />
+        <main>
+            < Banner />
+            < Sobre />
+        </main>
+        </>
+    )
+}
+export default App
---
+++ ds/turmas/2DES_A/4º Semestre/02-pfe2/aula03/cafeteria-react/src/components/Sobre.jsx
@@ -0,0 +1,16 @@
+function Sobre(){
+    return (
+        <section className="secao" id="sobre">
+            <div className="container sobre">
+                <div>
+                    <p className="destaque">Nossa História!</p>
+                    <h2>Mais que café, um lugar para se encontrar</h2>
+                    <p>Cafeteria Aroma nasceu pra oferecer um ambiente acolhedor,com cafés selecionados e atendimento próximo</p>
+                    <p> Nosso objetivo é transformar café em história!!</p>
+                </div>
+                <div className="ilustracao">☕</div>
+            </div>
+        </section>
+    )
+}
+export default Sobre

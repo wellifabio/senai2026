4afb76a4aee7576b014e6f31dc8476df3020b04f
robsonbsouzaa <robsonbacchindesouza@gmail.com>
2026-08-26 14:11:24 -0300

    up

---
+++ ds/turmas/2DES_B/4º Semestre/02-pfe2/aulao3/cafeteria-react/src/components/Cardapio.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { produtos } from '../data/produtos'
+// import CardProduto from './CardProduto'
+
+const categorias = ['Todos', 'Cafés', 'Bebidas','Doces']
+
+function Cardapio() {
+    const [categoriaAtiva, setCategoriaAtiva] = useState('Todos')
+
+    const produtosFiltrados = categoriaAtiva === 'Todos'
+    ? produtos
+    : produtos.filter(produto =>produto.categoria === categoriaAtiva)
+
+    return (
+        <section className="secao fundo-claro" id="cardapio">
+            <div className="container">
+                <p className="destaque centro">Escolha o seu favorito</p>
+                <h2 className="centro">Nosso Cardapio</h2>
+
+                <div className="filtros">
+                    {categorias.map((categoria)=>{
+                        <button key={categoria} 
+                        className={`filtro ${categoriaAtiva === categoria ? 'ativo':''}`}
+                        onClick={()=>setCategoriaAtiva(categoria)}>
+                        {categoria}
+                        </button>
+                    })}
+                </div>
+                <div className="grade">
+                    {produtosFiltrados.map((produto)=>{
+                        // <CardProduto key={produto.id} produto={produto} />
+                    <p>{produto.nome}</p>
+                    })}
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Cardapio
---
+++ ds/turmas/2DES_B/4º Semestre/02-pfe2/aulao3/cafeteria-react/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { use, useState } from "react";
+
+function Header(){
+    const [menuAberto, setMenuAberto] = useState(false)
+
+    function fecharMenu(){
+        setMenuAberto(false)
+    }
+
+    return (
+        <header className="cabecalho">
+            <div className="container menu">
+                <a className="logo" href="#inicio" onClick={fecharMenu}>☕ Aroma</a>
+                <button className="menu-botao" onClick={()=>setMenuAberto(!menuAberto)}>≡</button>
+                <nav className={`navegacao ${menuAberto ? 'aberta': ''}`}>
+                    <a href="#inicio" onClick={fecharMenu}>Inicio</a>
+                    <a href="#sobre" onClick={fecharMenu}>Sobre</a>
+                    <a href="#cardapio" onClick={fecharMenu}>Cardapio</a>
+                    <a href="#contato" onClick={fecharMenu}>Contato</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
+export default Header

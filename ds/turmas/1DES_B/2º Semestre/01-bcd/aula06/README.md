fd91b8498a47ff39aa84446c34961a18a198f8c6
wellifabio <wellifabio@hotmail.com>
2026-09-15 15:21:04 -0300

    aula03

--- ds/turmas/1DES_B/2º Semestre/01-bcd/aula06/README.md
+++ ds/turmas/1DES_B/2º Semestre/01-bcd/aula06/README.md
@@ -4,4 +4,27 @@
     - DDL
     - DML
 - Consultas simples
-## Desenvolva os Scripts para criar e popular o BD a Seguir
+## Desenvolva os Scripts para criar e popular o BD a Seguir:
+![MERDER](./amparo_taxi/merder.png)
+
+|MER DER Lógico|
+|-|
+|![MER DER Lógico](./amparo_taxi/der_logico.png)|
+## Atividade 01 - DDL
+- 1 Crie uma pasta na área de trabalho chamada `amparo_taxi` e abra com VsCode
+- 2 Crie um arquivo chamado `ddl.sql` e crie o script de criação do Banco de dados, tabelas e relacionamentos:
+```sql
+DROP DATABASE IF EXISTS amparo_taxi;
+CREATE DATABASE amparo_taxi;
+USE amparo_taxi;
+CREATE TABLE ...
+```
+- 3 Copie o script e cole no Shell do XAMPP ou MySQL WorkBanck
+## Atividade 02 - DML
+- 1 Crie um arquivo chamado `dml.sql` nesta mesma pasta.
+- 2 Crie um script que cadastre pelo menos 3 registros em cada tabela.
+```sql
+USE amparo_taxi;
+INSERT INTO motorista(...) values (),(),();
+```
+- Preferencialmente três viagens para cada passageiro.
--- ds/turmas/1DES_B/2º Semestre/01-bcd/aula06/amparo_taxi/dml.sql
+++ ds/turmas/1DES_B/2º Semestre/01-bcd/aula06/amparo_taxi/dml.sql
@@ -1,10 +1,10 @@
 USE amparo_taxi;
 INSERT INTO 
-    motorista(nome, cpf, cnh, email, status)
+    motorista(nome, cpf, cnh, celular, email, status)
 VALUES
-("Passos Dias Aguiar","127.330.356-30", "123456789", "19 99999-99990", "passos@email.com", "ATIVO"),
-("Juliana Aguiar","979.957.353-08", "987654321","19 99999-99991","juliana@email.com", "ATIVO"),
-("Ernesto Lakombi","334.595.611-06", "123123123","19 99999-99992","ernesto@email.com", "INATIVO");
+("Passos Dias Aguiar","127.330.356-30", "123456789", "19 99999-9990", "passos@email.com", "ATIVO"),
+("Juliana Aguiar","979.957.353-08", "987654321","19 99999-9991","juliana@email.com", "ATIVO"),
+("Ernesto Lakombi","334.595.611-06", "123123123","19 99999-9992","ernesto@email.com", "INATIVO");
 
 INSERT INTO
     veiculo(placa, modelo, marca, ano, cor, motorista_id)
@@ -12,3 +12,22 @@ VALUES
     ("AAA1A11","Yaris","Toyota",2025,"Branco",1),
     ("BBB1B11","Ecort","Ford",2010,"Branco",2),
     ("CCC1C11","Fiesta","Ford",2012,"Prata",3);
+
+INSERT INTO
+    passageiro
+VALUES
+(null,"Luciana do Passeio","333.116.387-27", "19 99999-0000","luciana@email.com","Só motorista mulher","ATIVO"),
+(null,"Viajando Henrique Cardoso","443.281.824-71", "19 99999-1111","viajando@email.com",null,"ATIVO"),
+(null,"Luis Messias","598.979.702-82", "19 99999-2222","luis@email.com",null,"INATIVO"),
+(null,"Flavio Marçal","470.510.453-69", "19 99999-3333","flavio@email.com",null,"ATIVO");
+
+INSERT INTO viagem
+VALUES
+(null, 1, "AAA1A11",8.49,"@-22.7142963,-46.8078886",CURTIME(),"@-22.7335338,-46.8713871",DATE_ADD(CURTIME(), INTERVAL 10 MINUTE),5,5),
+(null, 2, "AAA1A11",10.49,"@-22.7335338,-46.8713871",CURTIME(),"@-22.7079362,-46.9719265",DATE_ADD(CURTIME(), INTERVAL 8 MINUTE),4,5),
+(null, 4, "AAA1A11",7.29,"@-22.7079362,-46.9719265",CURTIME(),"@-22.6826759,-47.0010771",DATE_ADD(CURTIME(), INTERVAL 5 MINUTE),5,3),
+(null, 1, "BBB1B11",15.49,"@-22.7335338,-46.8713871",CURTIME(),"@-22.7079362,-46.9719265",DATE_ADD(CURTIME(), INTERVAL 16 MINUTE),5,5),
+(null, 2, "BBB1B11",9.49,"@-22.6826759,-47.0010771",CURTIME(),"@-22.7142963,-46.8078886",DATE_ADD(CURTIME(), INTERVAL 11 MINUTE),2,2),
+(null, 4, "BBB1B11",18.29,"@-22.7079362,-46.9719265",CURTIME(),"@-22.7335338,-46.8713871",DATE_ADD(CURTIME(), INTERVAL 14 MINUTE),5,3);
+
+

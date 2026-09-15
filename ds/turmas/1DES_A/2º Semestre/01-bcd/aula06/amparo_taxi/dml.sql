50a82beaa5dc264837f213099c6aaaee49fc9498
wellifabio <wellifabio@hotmail.com>
2026-09-15 17:02:49 -0300

    aula06

---
+++ ds/turmas/1DES_A/2º Semestre/01-bcd/aula06/amparo_taxi/dml.sql
@@ -0,0 +1,6 @@
+USE amparo_taxi;
+INSERT INTO motorista
+VALUES
+(null, "Passos Dias Aguiar", "111.222.333-11","123456789","19 99999-9999","passos@email.com",null,"ATIVO"),
+(null, "Romero Aguiar", "222.222.333-22","987654321","19 99999-8888","romero@email.com",null,"ATIVO"),
+(null, "Jacinto Aguiar", "333.222.333-33","123654777","19 99999-7777","jacinto@email.com","Acusado de assédio","INATIVO");
